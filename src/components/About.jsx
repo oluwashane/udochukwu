c1ab6ebe4c735e5b4c7bfbb50a8186064fc76e99
oluwashane <ogbulaforudochukwu@gmail.com>
2020-11-02 23:23:10 +0100

    bug fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,19 +19,16 @@ const About = () => {
   return (
     <div className="aboutMe">
       <h1>About</h1>
-      {/* <div className="avatar">
-        <img src={myPicture} alt="avatar" />
-      </div> */}
       <p>
       I would have to skip the boring part and go straight to the point. 
       I'm a goal-driven developer that focuses on getting the work done with minimum error. 
       Over the years of learning and development, I have been opportune to learn from the best and work as a team member to contribute and find solutions to problems. 
-      Right now, I'm currently building my stack on Javascript.
+      Right now, I'm currently working with MERN stack.
       </p>
       {skills}
       <div className="contactMe">
         <a class="underline-button text-color" rel="noopener noreferrer" target="_blank" href="https://github.com/oluwashane">Github</a>
-        <a className="underline-button text-color" rel="noopener noreferrer" target="_blank" href="mailto:hi@jonasf.work">Email</a>
+        <a className="underline-button text-color" rel="noopener noreferrer" target="_blank" href="mailto:ogbulaforudochukwu@gmail.com">Email</a>
       </div>
       
 </div>
